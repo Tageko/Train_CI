2b2db2a08ea27ea1b06ad9a434a3d63b9b88077e
Tatyana Kopina <tatyanakopina@gmail.com>
2021-04-06 15:55:04 +0300

    added findMax2

--- src/test/java/ru/netology/stats/StatisticsServiceTest.java
+++ src/test/java/ru/netology/stats/StatisticsServiceTest.java
@@ -14,4 +14,13 @@ class StatisticsServiceTest {
         long actual = service.findMax(incomesInBillions);
         assertEquals(expected, actual);
     }
+
+    @Test
+    void findMax2() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 15};
+        long expected = 15;
+        long actual = service.findMax(incomesInBillions);
+        assertEquals(expected, actual);
+    }
 }
